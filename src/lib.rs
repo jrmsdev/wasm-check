8e40468f32be9a99d97a933925ecd10207233089
Jeremias <jrmsdev@gmail.com>
2024-08-22 15:45:17 -0300

    web-sys dep

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use wasm_bindgen::prelude::*;
 
+use web_sys::console;
+
 #[wasm_bindgen]
 pub fn wc_init() -> String {
+    console::log_1(&"wc_init".into());
     "WASM application loaded".to_string()
 }
