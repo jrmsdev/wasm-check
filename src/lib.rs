9e776206c205b9d2dbfc24a78712c701b378eacf
Jeremias <jrmsdev@gmail.com>
2024-08-22 12:34:58 -0300

    html index

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ use wasm_bindgen::prelude::*;
 // Expose the `greet` function to JavaScript
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+    format!("Hello {}!", name)
 }
