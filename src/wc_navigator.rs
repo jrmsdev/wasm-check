83b6bf01074d6c112520d6e3288c0971e13e4536
Jeremias <jrmsdev@gmail.com>
2024-08-23 15:27:53 -0300

    add navigator info

--- src/wc_navigator.rs
+++ src/wc_navigator.rs
@@ -18,8 +18,13 @@ pub fn wc_navigator() -> Result<JsValue, JsValue> {
     let n = window().unwrap().navigator();
     //~ let n2 = n.as_ref();
     let data = vec![
-        SystemInfo { name: "User Agent".into(),
-            value: n.user_agent().unwrap()},
+        SystemInfo { name: "App Name".into(), value: n.app_name()},
+        SystemInfo { name: "App Version".into(), value: n.app_version().unwrap()},
+        SystemInfo { name: "Platform".into(), value: n.platform().unwrap()},
+        SystemInfo { name: "User Agent".into(), value: n.user_agent().unwrap()},
+        SystemInfo { name: "Product".into(), value: n.product()},
+        SystemInfo { name: "Language".into(), value: n.language().unwrap()},
+        SystemInfo { name: "On Line".into(), value: n.on_line().to_string()},
     ];
     // Return the data as a JSON object
     Ok(serde_wasm_bindgen::to_value(&data)?)
