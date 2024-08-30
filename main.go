5903238ec3ee57f7f61039865d8f12a3607d9a04
Jeremías <jrmsdev@gmail.com>
2024-08-30 14:43:56 -0300

    show runtime version

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 // Copyright Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
+// https://go.dev/wiki/WebAssembly
+
 package main
 
 import (
 	"fmt"
+	"runtime"
 	"syscall/js"
 )
 
 func main() {
 
-	wc_init := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		return "OK"
+	// wc_init
+	wc_init := js.FuncOf(func(this js.Value, args []js.Value) any {
+		return runtime.Version()
 	})
 	defer wc_init.Release()
 	js.Global().Set("wc_init", wc_init)
 
 	fmt.Println("wasm-check loaded")
-
-	// infinite loop
 	select {}
 }
