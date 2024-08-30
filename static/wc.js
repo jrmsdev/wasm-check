804b64a6e177b5b3f08a14cb3637a87e4aea8237
Jeremías <jrmsdev@gmail.com>
2024-08-30 17:31:01 -0300

    fix wasm URL

--- static/wc.js
+++ static/wc.js
@@ -1,7 +1,7 @@
 import '../pkg/wasm_exec.js';
 
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch('../pkg/main.wasm'), go.importObject).then((result) => {
+WebAssembly.instantiateStreaming(fetch('./pkg/main.wasm'), go.importObject).then((result) => {
 	go.run(result.instance);
 	document.getElementById("wc_init").textContent = wc_init();
 });
